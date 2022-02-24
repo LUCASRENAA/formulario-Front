f8cc5d592a1540c8252c745c8f5354f782f8104e
RaphaelAlcantara <rvfra@discente.ifpe.edu.br>
2022-02-24 16:13:15 -0300

    Form finalizado

--- script.js
+++ script.js
@@ -1,15 +1,66 @@
-
-
 const init = () => {
+    //ValidaEmail
+    const validateEmail = (event) =>{
+        const input = event.currentTarget;
+        const regex =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+        const emailTeste = regex.test(input.value);
+
+        if (!emailTeste){
+            sumitButton.setAttribute('disabled', 'disabled');
+            input.nextElementSibling.classList.add('error');
+        }else{
+            sumitButton.removeAttribute('disabled');
+            input.nextElementSibling.classList.remove('error')
+        }
+    }
+    //Valida Password
+    const validatePassword = (event) => {
+        const input = event.currentTarget;
+
+        if (input.value.length < 8){
+            sumitButton.setAttribute('disabled', 'disabled');
+            input.nextElementSibling.classList.add('error');
+        }else{
+            sumitButton.removeAttribute('disabled');
+            input.nextElementSibling.classList.remove('error')
+        }
+    }
+
+    //Get dos campos
     const inputEmail = document.querySelector('input[type="email"]');
     const inputPassword = document.querySelector('input[type="password"]');
     const sumitButton = document.getElementById("sumit");
+
+    //Chama funções de validações
+    inputEmail.addEventListener('input', validateEmail);
+    inputPassword.addEventListener('input', validatePassword);
+
+    //Feedback do usuario para login inválido
+    const erroHandler = () => {
+        sumitButton.classList.remove('success');
+        sumitButton.classList.add('error');
+        sumitButton.textContent = 'LOGIN INVÁLIDO'
+    }
+    //Feedback do usuario para login válido
+    const sucessHandler = () => {
+        sumitButton.classList.remove('error');
+        sumitButton.classList.add('success');
+        sumitButton.textContent = 'LOGIN OK'
+    }
 
     if(sumitButton) {
         sumitButton.addEventListener('click', (event) => {
             event.preventDefault();
 
-            //A API utilizada é a Reqres para testes
+            sumitButton.textContent = '...Aguarde';
+
+            /*
+            A API utilizada é a Reqres para testes e Credeciais para login:
+
+            Email = eve.holt@reqres.in
+            Password = cityslicka
+            Token return 200 = QpwL5tke4Pnpja7X4
+             */
             fetch('https://reqres.in/api/login',{
                 method: 'POST',
                 headers: {
@@ -19,11 +70,17 @@ const init = () => {
                     email: inputEmail.value,
                     password: inputPassword.value
                 })//Callback para quando fetch return 200
-                 }).then((response)=> {
-                    return response.json();
-            }).then((data) => {
+            }).then((response)=> {
+                    if (response.status !==200){
+                        return erroHandler();
+                    }
+                    sucessHandler();
+            })/*.then((data) => {
                 console.log(data)
-            })
+            })*/
+                .catch(()=>{
+                 erroHandler();
+                })
 
         })
 
